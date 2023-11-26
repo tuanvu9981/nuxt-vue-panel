6580dada648893468d2e52ae1f179f094b4185a1
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-11-26 22:12:04 +0900

    feat: complete login, signup with firebase

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,35 +1,16 @@
 <template>
-    <v-card
-        class="mx-auto my-16"
-        max-width="450"
-        max-height="450"
-        elevation="5"
-    >
+    <v-card class="mx-auto my-16" max-width="450" max-height="450" elevation="5">
         <v-card-title class="text-h5 text-center mt-4 font-weight-bold text-blue text-lighten-3">飲食店管理画面</v-card-title>
         <p class="text-h7 text-center my-3 font-weight-bold">
             ユーザログイン
         </p>
         <v-container>
-            <v-text-field
-                v-model="email"
-                label="メールアドレス"
-                variant="outlined"
-                :error="errorEmail"
-                :error-messages="errEmailMsg"
-            ></v-text-field>
+            <v-text-field v-model="email" label="メールアドレス" variant="outlined" :error="errorEmail"
+                :error-messages="errEmailMsg"></v-text-field>
 
-            <v-text-field
-                class="mt-5"
-                :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="visible ? 'text': 'password'"
-                @click:append="visible = !visible"
-                v-model="password"
-                label="パスワード"
-                variant="outlined"
-                :error="errorPw"
-                :error-messages="errPwMsg"
-
-            ></v-text-field>
+            <v-text-field class="mt-5" :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="visible ? 'text' : 'password'" @click:append="visible = !visible" v-model="password" label="パスワード"
+                variant="outlined" :error="errorPw" :error-messages="errPwMsg"></v-text-field>
 
             <p class="text-right font-weight-medium">
                 アカウントを持っていません？<NuxtLink to="/sign-up" class="text-green text-lighten-3">サインアップ</NuxtLink>
@@ -47,6 +28,7 @@
 <script>
 import { ref } from 'vue';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { app } from '../firebase/config';
 import { FirebaseError } from '@firebase/util'
 
 export default {
@@ -61,19 +43,20 @@ export default {
         const errPwMsg = ref('');
 
         const signIn = async () => {
-            if (email.value === '' || !email.value.includes('@')){
+            if (email.value === '' || !email.value.includes('@')) {
                 errorEmail.value = true;
                 errEmailMsg.value = 'メールは空あるいは形式が違います';
-                if (password.value === ''){
+                if (password.value === '') {
                     errorEmail.value = true;
                     errPwMsg.value = 'パスワードは空です'
                 }
                 return
-            } 
-            
+            }
+
             try {
-                const auth = getAuth()
-                await signInWithEmailAndPassword(auth, email, password)
+                const auth = getAuth(app)
+                await signInWithEmailAndPassword(auth, email.value, password.value)
+                await navigateTo('/')
             } catch (e) {
                 if (e instanceof FirebaseError) {
                     console.log(e)
--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -1,62 +1,34 @@
 <template>
-    <v-card
-        class="mx-auto my-16"
-        max-width="450"
-        elevation="5"
-    >
+    <v-card class="mx-auto my-16" max-width="450" elevation="5">
         <v-card-title class="text-h5 text-center mt-4 font-weight-bold text-blue text-lighten-3">飲食店管理画面</v-card-title>
         <p class="text-h7 text-center my-3 font-weight-bold">
             ユーザサインアップ
         </p>
         <v-container>
-            <v-text-field
-                v-model="email"
-                label="メールアドレス"
-                variant="outlined"
-                :error="errorEmail"
-                :error-messages="errEmailMsg"
-            ></v-text-field>
+            <v-text-field v-model="email" label="メールアドレス" variant="outlined" :error="errorEmail"
+                :error-messages="errEmailMsg"></v-text-field>
 
-            <v-text-field
-                class="mt-5"
-                :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="visible ? 'text': 'password'"
-                @click:append="visible = !visible"
-                v-model="password"
-                label="パスワード"
-                variant="outlined"
-                :error="errorPw"
-                :error-messages="errPwMsg"
-
-            ></v-text-field>
+            <v-text-field class="mt-5" :append-inner-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+                :type="visible ? 'text' : 'password'" @click:append="visible = !visible" v-model="password" label="パスワード"
+                variant="outlined" :error="errorPw" :error-messages="errPwMsg"></v-text-field>
 
             <p class="text-right font-weight-medium">
                 アカウントを持っています？<NuxtLink to="/sign-in" class="text-green text-lighten-3">ログイン</NuxtLink>
             </p>
 
-            <v-checkbox 
-                v-model="checked" 
-                color="success" 
-                :error="errorChecked"
-                :error-messages="errCheckbox"
-            >
+            <v-checkbox v-model="checked" color="success" :error="errorChecked" :error-messages="errCheckbox">
                 <template v-slot:label>
                     <div>
                         私は
-                    <v-tooltip location="bottom">
-                        <template v-slot:activator="{ props }">
-                            <a
-                                target="_blank"
-                                href="https://vuetifyjs.com"
-                                v-bind="props"
-                                @click.stop
-                            >
-                                利用条件
-                            </a>
-                        </template>
-                        新しいウィンドウで開く
-                    </v-tooltip>
-                    に合意する
+                        <v-tooltip location="bottom">
+                            <template v-slot:activator="{ props }">
+                                <a target="_blank" href="https://vuetifyjs.com" v-bind="props" @click.stop>
+                                    利用条件
+                                </a>
+                            </template>
+                            新しいウィンドウで開く
+                        </v-tooltip>
+                        に合意する
                     </div>
                 </template>
             </v-checkbox>
@@ -73,11 +45,12 @@
 <script>
 import { ref } from 'vue';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { app } from '../firebase/config';
 import { FirebaseError } from '@firebase/util'
 
 export default {
     name: "SignUp",
-    setup(){
+    setup() {
         const email = ref('');
         const password = ref('');
         const visible = ref(false);
@@ -90,23 +63,24 @@ export default {
         const checked = ref(false)
 
         const signUp = async () => {
-            if (email.value === '' || !email.value.includes('@')){
+            if (email.value === '' || !email.value.includes('@')) {
                 errorEmail.value = true;
                 errEmailMsg.value = 'メールは空あるいは形式が違います';
-                if (password.value === ''){
+                if (password.value === '') {
                     errorEmail.value = true;
                     errPwMsg.value = 'パスワードは空です'
                 }
-                if (!checked.value){
+                if (!checked.value) {
                     errorChecked.value = true;
                     errCheckbox.value = '利用条件チェックしてください'
                 }
                 return
-            } 
+            }
             try {
-                const auth = getAuth()
-                const user = await createUserWithEmailAndPassword(auth, email, password)
-                // user: UserCredential
+                const auth = getAuth(app);
+                const user = await createUserWithEmailAndPassword(auth, email.value, password.value)
+                // console.log(user);
+                await navigateTo('/')
             } catch (e) {
                 if (e instanceof FirebaseError) {
                     console.log(e)
@@ -128,5 +102,4 @@ export default {
 }
 </script>
 
-<style>
-</style>
+<style></style>
